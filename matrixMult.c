8d815bdbf92e3679fe65da1447cf1e733568d521
MelroyNoronha <melmax1208@gmail.com>
2017-09-16 13:18:40 +0530

    defined getValues()

--- matrixMult.c
+++ matrixMult.c
@@ -1,46 +1,72 @@
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
 //Global vars
 int matrix1[25][25], matrix2[25][25], result[25][25];
-int   r1,c1,r2,c2,i,j,k;
+int r1, c1, r2, c2, i, j, enteredValue; //throughout the program i and j are variables reserved for use in for loops
 
-int initSetup(){
+int initSetup()
+{
 	printf("Enter the no. of rows in the first matrix and press the return key then proceed to enter the no. of coloumns \n");
-	scanf("%d",&r1);
+	scanf("%d", &r1);
 	printf("x\n");
-	scanf("%d",&c1); 
+	scanf("%d", &c1);
 
 	printf("Enter the no. of rows in the second matrix and press the return key then proceed to enter the no. of coloumns \n");
-	scanf("%d",&r2);
+	scanf("%d", &r2);
 	printf("x\n");
-	scanf("%d",&c2);
+	scanf("%d", &c2);
 }
 
-int errHandle(){
+int handleErr()
+{
 	printf("Error! the matrices cannot be multiplied\n\nEnter new values\n");
 	initSetup();
 }
 
-int checkForErr(){
-	if(c1!=r2){
+int checkForErr()
+{
+	if (c1 != r2)
+	{
 		return true;
 	}
 }
 
-getValues(){
-	//pending
+getValues()
+{
+	printf("\nEnter values in the first matrix(%dx%d)\n", r1, c1);
+	for(i = 1; i <= r1; i++)
+	{
+		for(j = 1; j <= c1; j++)
+		{
+			printf("Element no.%d%d: ", i, j);
+			scanf("%f",&enteredValue);
+			matrix1[i][j] += enteredValue;
+		}
+	}
+
+	printf("\nEnter values in the second matrix(%dx%d)\n", r2, c2);
+	for(i = 1; i <= r2; i++)
+	{
+		for(j = 1; j <= c2; j++)
+		{
+			printf("Element no.%d%d: ", i, j);
+			scanf("%f",&enteredValue);
+			matrix2[i][j] += enteredValue;
+		}
+	}
 }
 
 int main()
 {
 	initSetup();
-	
-	if(checkForErr() == true){
-		errHandle();
+
+	if (checkForErr() == true)
+	{
+		handleErr();
 	};
-	
+
 	getValues();
-	 
+
 	return 0;
 }
